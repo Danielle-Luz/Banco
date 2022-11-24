0041db0713592948dd7ef957b8d1aed9635ccb83
Axionauta <daniln2209@gmail.com>
2022-11-23 21:49:17 -0300

    feat: adiciona instância de conexão na classe pessoa física

--- src/br/com/mesttra/banco/dao/PessoaFisicaDAO.java
+++ src/br/com/mesttra/banco/dao/PessoaFisicaDAO.java
@@ -1,5 +1,11 @@
 package br.com.mesttra.banco.dao;
 
+import java.sql.Connection;
+
+import br.com.mesttra.banco.connectionfactory.ConnectionFactory;
+
 public class PessoaFisicaDAO {
-  
+  Connection connection = ConnectionFactory.getConnection();
+
+
 }
