5432a9b476344cdc1b6a6a5a9824e8a6cd817963
Devluck-k <devluck.2k@gmail.com>
2022-11-23 21:38:45 -0300

    Commitando ConnectionFactory

--- src/br/com/mesttra/banco/connectionfactory/ConnectionFactory.java
+++ src/br/com/mesttra/banco/connectionfactory/ConnectionFactory.java
@@ -1,5 +1,16 @@
 package br.com.mesttra.banco.connectionfactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
-  
+	public static Connection getConnection() {
+		try {
+			return DriverManager.getConnection("");
+		} catch (SQLException e) {
+			System.err.println("Erro de conexão");
+			return null;
+		}
+	}
 }
