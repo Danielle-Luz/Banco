d8d0162e3e80e7d0b502720c930c03811a7b450d
Axionauta <daniln2209@gmail.com>
2022-11-23 23:17:46 -0300

    feat: adiciona métodos de leitura de dados

---
+++ src/br/com/mesttra/banco/scanner/Scanner.java
@@ -0,0 +1,66 @@
+package br.com.mesttra.banco.scanner;
+
+import java.util.InputMismatchException;
+
+public class Scanner {
+
+  public String lerValorAlfanumerico(String mensagem) {
+    java.util.Scanner scan = new java.util.Scanner(System.in);
+
+    System.out.print(mensagem);
+
+    String valor = scan.nextLine();
+
+    return valor;
+  }
+
+  public double lerValorMonetario(String mensagem) {
+    double valor = 0;
+
+    do {
+      java.util.Scanner scan = new java.util.Scanner(System.in);
+
+      try {
+        System.out.print(mensagem);
+
+        valor = scan.nextFloat();
+
+        break;
+      } catch (InputMismatchException ex) {
+        System.out.println("Insira um valor monetário válido.");
+      }
+    } while (true);
+
+    return valor;
+  }
+
+  public int lerValorInteiro(String mensagem) {
+    int valor = 0;
+
+    do {
+      try {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+
+        System.out.print(mensagem);
+
+        valor = scan.nextInt();
+
+        break;
+      } catch (InputMismatchException ex) {
+        System.out.println("Insira um número inteiro válido.");
+      }
+    } while (true);
+
+    return valor;
+  }
+
+  public int lerValorInteiroComLimites (int min, int max, String mensagem) {
+    while (true) {
+      int valor = lerValorInteiro(mensagem);
+
+      if ((valor >= min) && (valor <= max)) return valor;
+
+      System.out.printf("Digite um valor entre %d e %d\n", min, max);
+    }
+  }
+}
