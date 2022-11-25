3894e177b1436d5321377e11e7a569e1d7b390f0
Axionauta <daniln2209@gmail.com>
2022-11-25 00:38:07 -0300

    feat: adiciona método de atualização genérico de transferência de saldo

--- src/br/com/mesttra/banco/dao/ClienteDAO.java
+++ src/br/com/mesttra/banco/dao/ClienteDAO.java
@@ -1,52 +1,57 @@
 package br.com.mesttra.banco.dao;
 
+import br.com.mesttra.banco.connectionfactory.ConnectionFactory;
+import br.com.mesttra.banco.pojo.ClientePojo;
+import br.com.mesttra.banco.pojo.PessoaFisicaPojo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import br.com.mesttra.banco.connectionfactory.ConnectionFactory;
-
-import br.com.mesttra.banco.pojo.ClientePojo;
-import br.com.mesttra.banco.pojo.PessoaFisicaPojo;
-
 public class ClienteDAO {
-	
-  static private Connection conexao = ConnectionFactory.getConnection();
-	
-	static public boolean transfereSaldo (ClientePojo contaTransferidora, ClientePojo contaReceptora, float valor) {
-		String transferidor = contaTransferidora instanceof PessoaFisicaPojo ? "pessoa_fisica" : "pessoa_juridica";
-		String receptor = contaReceptora instanceof PessoaFisicaPojo ? "pessoa_fisica" : "pessoa_juridica";
-		
-		String transfere = "UPDATE " + transferidor + " SET saldo = (saldo - ?) WHERE numeroConta = " + contaTransferidora.getNumeroConta() + ";";
-		String recebe = "UPDATE " + receptor + " SET saldo = (saldo + ?) WHERE numeroConta = " + contaReceptora.getNumeroConta() + ";";
-		
-		if (contaTransferidora.getSaldo() < valor) {
-			System.err.println("Saldo insuficiente");
-			return false;
-		} else {
-			
-			try {
-				PreparedStatement stt1 = conexao.prepareStatement(transfere);
-				stt1.setFloat(1, valor);
-				stt1.execute();
-				stt1.close();
-				PreparedStatement stt2 = conexao.prepareStatement(recebe);
-				stt2.setFloat(1, valor);
-				stt2.execute();
-				stt2.close();
-				System.out.println("\n[Transferência realizada com sucesso]\n");
-				return true;
-			} catch (SQLException e) {
-				System.err.println("\n[Erro na transferência]\n");
-				return false;
-			}
-		}
-	}
 
-  static public void atualizarCliente (ClientePojo cliente, Object valor, String coluna) {
-    String nomeTabela = cliente instanceof PessoaFisicaPojo ? "pessoa_fisica" : "pessoa_juridica";
+  private static Connection conexao = ConnectionFactory.getConnection();
 
-    String comando = String.format("UPDATE %s SET %s = ? WHERE numeroConta = '%s'", nomeTabela, coluna, cliente.getNumeroConta());
+  public static boolean transfereSaldo(
+    ClientePojo contaTransferidora,
+    ClientePojo contaReceptora,
+    float valor
+  ) {
+    if (contaTransferidora.getSaldo() < valor) {
+      System.err.println("Saldo insuficiente");
+    } else {
+      atualizarCliente(
+        contaTransferidora,
+        contaTransferidora.getSaldo() - valor,
+        "saldo"
+      );
+      atualizarCliente(
+        contaReceptora,
+        contaReceptora.getSaldo() + valor,
+        "saldo"
+      );
+
+      System.out.println("\n[Transferência realizada com sucesso]\n");
+      return true;
+    }
+
+    return false;
+  }
+
+  public static void atualizarCliente(
+    ClientePojo cliente,
+    Object valor,
+    String coluna
+  ) {
+    String nomeTabela = cliente instanceof PessoaFisicaPojo
+      ? "pessoa_fisica"
+      : "pessoa_juridica";
+
+    String comando = String.format(
+      "UPDATE %s SET %s = ? WHERE numeroConta = '%s'",
+      nomeTabela,
+      coluna,
+      cliente.getNumeroConta()
+    );
 
     try (PreparedStatement sql = conexao.prepareStatement(comando)) {
       if (valor instanceof String) {
